5f1530269292bcb1a7a867fa117dc5f9d8a2b428
Zhanchao Yang <zyang91@binghamton.edu>
2023-04-11 16:47:34 -0400

    added new function

--- geomapdemo/geomapdemo.py
+++ geomapdemo/geomapdemo.py
@@ -1,1 +1,9 @@
 """Main module."""
+
+import random
+import string
+
+def generate_random_string(length):
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join(random.choice(letters_and_digits) for i in range(length))
+    return result_str
