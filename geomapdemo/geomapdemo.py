9509f58928f6ee309e20cc1d2bc052628d15a45d
Zhanchao Yang <zyang91@binghamton.edu>
2023-04-16 18:39:08 -0400

    added shapefile function

--- geomapdemo/geomapdemo.py
+++ geomapdemo/geomapdemo.py
@@ -36,6 +36,50 @@ class Map(ipyleaflet.Map):
             """Adds a fullscreen control to the map"""
             self.add_fullscreen_control()
 
+        if "search_control" not in kwargs:
+            """Adds a search control by default"""
+            kwargs["search_control"] = True
+        
+        if kwargs["search_control"]:
+            """Adds a search control to the map"""
+            self.add_search_control()
+
+
+    def add_basemap(self, basemap, **kwargs):
+        """Adds a basemap to the map
+        Args:
+            basemap(str): The name of basemap.
+            **kwargs: Keyword arguments to be passed to the basemap.
+        """
+
+        import xyzservices.providers as xyz
+
+        if basemap.lower() =='roadmap':
+            url = 'http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}'
+            self.add_tile_layer(url, basemap, **kwargs)
+
+        elif basemap.lower() =='satellite':
+            url = 'http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}'
+            self.add_tile_layer(url, basemap, **kwargs)
+        
+        elif basemap.lower() =='esri':
+            url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+            self.add_tile_layer(url, basemap, **kwargs)
+        
+        else:
+            try:
+                basemap =eval(f"xyz.{basemap}")
+                url=basemap.build_url()
+                attribution=basemap.attribution
+                self.add_tile_layer(url, name=basemap.name, attribution= attribution, **kwargs)
+            except:
+                raise ValueError(f"{basemap} is not a valid basemap")
+
+
+
+
+        
+    
     def add_tile_layer(self, url, name, attribution="", **kwargs):
         """Adds a tile layer to the map
         Args:
@@ -44,14 +88,10 @@ class Map(ipyleaflet.Map):
             attribution (str, optional): The attribution of the tile layer. Defaults to "".
             **kwargs: Keyword arguments to be passed to the tile layer.
         """        
-        tile_layer = ipyleaflet.TileLayer(
-            url=url, 
-            name = name,
-            attribution = attribution,
-            **kwargs
-        )
+        tile_layer = ipyleaflet.TileLayer(url=url, name=name, attribution=attribution, **kwargs)
         self.add_layer(tile_layer)
     
+
     def add_layers_control(self, position ="topright", **kwargs):
         """Adds a layers control to the map
         Args:
@@ -77,8 +117,8 @@ class Map(ipyleaflet.Map):
             **kwargs: Keyword arguments to be passed to the search control.
         """  
         if "url" not in kwargs:
-            """Sets the default url for the search control"""
-            kwargs["url"] = "https://nominatim.openstreetmap.org/search?format=json&q={s}"     
+             """Sets the default url for the search control"""
+             kwargs["url"] = "https://nominatim.openstreetmap.org/search?format=json&q={s}"    
         search_control = ipyleaflet.SearchControl(position=position, **kwargs)
         self.add_control(search_control)
 
@@ -90,39 +130,67 @@ class Map(ipyleaflet.Map):
         draw_control = ipyleaflet.DrawControl(**kwargs)
 
         draw_control.polyline =  {
-        "shapeOptions": {
-            "color": "#6bc2e5",
-            "weight": 8,
-            "opacity": 1.0
-            }   
-        }
+            "shapeOptions": {
+                "color": "#6bc2e5",
+                "weight": 8,
+                "opacity": 1.0
+                }   
+            }
         draw_control.polygon = {
-            "shapeOptions": {
-                "fillColor": "#6be5c3",
-                "color": "#6be5c3",
-                "fillOpacity": 1.0
-            },
-            "drawError": {
-                "color": "#dd253b",
-                "message": "Oups!"
-            },
-            "allowIntersection": False
-        }
+                "shapeOptions": {
+                    "fillColor": "#6be5c3",
+                    "color": "#6be5c3",
+                    "fillOpacity": 1.0
+                },
+                "drawError": {
+                    "color": "#dd253b",
+                    "message": "Oups!"
+                },
+                "allowIntersection": False
+            }
         draw_control.circle = {
-            "shapeOptions": {
-                "fillColor": "#efed69",
-                "color": "#efed69",
-                "fillOpacity": 1.0
+                "shapeOptions": {
+                    "fillColor": "#efed69",
+                    "color": "#efed69",
+                    "fillOpacity": 1.0
+                }
             }
-        }
         draw_control.rectangle = {
-            "shapeOptions": {
-                "fillColor": "#fca45d",
-                "color": "#fca45d",
-                "fillOpacity": 1.0
+                "shapeOptions": {
+                    "fillColor": "#fca45d",
+                    "color": "#fca45d",
+                    "fillOpacity": 1.0
+                }
             }
-        }
         self.add_control(draw_control)
+    
+    def add_geojson(self, data, name ='GeoJson', **kwargs):
+        """Adds a geojson to the map
+        Args:
+            data (dict): The geojson data.
+            name (str, optional): The name of the geojson. Defaults to 'GeoJson'.
+            **kwargs: Keyword arguments to be passed to the geojson.
+        """
+        if isinstance(data, str):
+            """If the data is a string, it is assumed to be a path to a geojson file"""
+            import json
+            with open(data, "r") as f:
+                data = json.load(f)
+          
+        geojson = ipyleaflet.GeoJSON(data=data, name=name, **kwargs)
+        self.add_layer(geojson)
+    
+    def add_shp(self, path, name ='Shapefile', **kwargs):
+        """Adds a shapefile to the map
+        Args:
+            path (str): The path to the shapefile.
+            name (str, optional): The name of the shapefile. Defaults to 'Shapefile'.
+            **kwargs: Keyword arguments to be passed to the shapefile.
+        """        
+        import geopandas as gpd
+        gdf = gpd.read_file(path)
+        geojson = gdf.__geo_interface__
+        self.add_geojson(geojson, name=name, **kwargs)
 
 
 
