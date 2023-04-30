44c78ebbb1f40f909fe49573aa7da3a98df7a7d0
zyang91 <zyang91@binghamton.edu>
2023-04-29 20:53:52 -0400

    updated function

--- geomapdemo/chart.py
+++ geomapdemo/chart.py
@@ -29,3 +29,32 @@ def scatter_plot(data, x, y, hue= None, **kwargs):
         df = data
     sns.catplot(data=df, x=x, y=y, hue=hue, **kwargs)
 
+def violin_plot(data, x, y, hue= None, **kwargs):
+    '''Plot data and regression model fits across a FacetGrid.
+    Args:
+        data (DataFrame): CSV file path or DataFrame object.
+        x, y (str): Variables that specify positions on the x and y axes.
+        hue (str): Variable in data to map plot aspects to different colors.
+        kwargs: Additional keyword arguments are passed to the function used to draw the plot on the joint Axes, superseding items in the joint_kws dictionary.
+    '''
+    if isinstance(data, str):
+        df = pd.read_csv(data)
+    else:
+        df = data
+    sns.violinplot(data=df, x=x, y=y, hue=hue, **kwargs)
+
+def violin_spilt(data, x, y, hue, spilt= True, **kwargs):
+    '''Plot data and regression model fits across a FacetGrid.
+    Args:
+        data (DataFrame): CSV file path or DataFrame object.
+        x, y (str): Variables that specify positions on the x and y axes.
+        hue (str): Variable in data to map plot aspects to different colors.
+        spilt (bool): Whether to draw half of a violin for each hue level or allow them to overlap.
+        kwargs: Additional keyword arguments are passed to the function used to draw the plot on the joint Axes, superseding items in the joint_kws dictionary.
+    '''
+    if isinstance(data, str):
+        df = pd.read_csv(data)
+    else:
+        df = data
+    sns.violinplot(data=df, x=x, y=y, hue=hue, split=spilt, **kwargs)
+
