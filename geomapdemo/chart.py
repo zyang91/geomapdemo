f1c6897c42b8b1dd2dcc10828c0811106267d77f
zyang91 <zyang91@binghamton.edu>
2023-05-01 14:09:57 -0400

    update function

--- geomapdemo/chart.py
+++ geomapdemo/chart.py
@@ -68,5 +68,17 @@ def single_violin(data, variable, **kwargs):
         df = data
     sns.violinplot(x=df[variable], **kwargs)
 
-
+def box_plot(data, x, y, hue= None, **kwargs):
+    '''Plot data and regression model fits across a FacetGrid.
+    Args:
+        data (DataFrame): CSV file path or DataFrame object.
+        x, y (str): Variables that specify positions on the x and y axes.
+        hue (str): Variable in data to map plot aspects to different colors.
+        kwargs: Additional keyword arguments are passed to the function used to draw the plot on the joint Axes, superseding items in the joint_kws dictionary.
+    '''
+    if isinstance(data, str):
+        df = pd.read_csv(data)
+    else:
+        df = data
+    sns.boxplot(data=df, x=x, y=y, hue=hue, **kwargs)
 
