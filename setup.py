36a508a42ed6d2193af902249b7d321df57f3abf
zyang91 <zyang91@binghamton.edu>
2023-05-07 15:15:23 -0400

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zyang91/geomapdemo',
-    version='0.3.1',
+    version='0.3.2',
     zip_safe=False,
 )
