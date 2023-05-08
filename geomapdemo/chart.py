72ed21bc65eda3e5cecb75d3a32b3b1b76a4db4d
zyang91 <zyang91@binghamton.edu>
2023-05-08 17:33:44 -0400

    update chart function

--- geomapdemo/chart.py
+++ geomapdemo/chart.py
@@ -109,3 +109,29 @@ def bar_plot(data, x, y, hue= None, **kwargs):
         df = data
     sns.barplot(data=df, x=x, y=y, hue=hue, **kwargs)
 
+def single_bar_plot(data, variable, **kwargs):
+    '''Plot data and regression model fits across a FacetGrid.
+    Args:
+        data (DataFrame): CSV file path or DataFrame object.
+        variable (str): Variables that specify positions on the x and y axes.
+        kwargs: Additional keyword arguments are passed to the function used to draw the plot on the joint Axes, superseding items in the joint_kws dictionary.
+    '''
+    if isinstance(data, str):
+        df = pd.read_csv(data)
+    else:
+        df = data
+    sns.barplot(data=df,y=df[variable], **kwargs)
+
+def count_plot(data, x, hue= None, **kwargs):
+    '''Plot data and regression model fits across a FacetGrid.
+    Args:
+        data (DataFrame): CSV file path or DataFrame object.
+        x (str): Variables that specify positions on the x and y axes.
+        hue (str): Variable in data to map plot aspects to different colors.
+        kwargs: Additional keyword arguments are passed to the function used to draw the plot on the joint Axes, superseding items in the joint_kws dictionary.
+    '''
+    if isinstance(data, str):
+        df = pd.read_csv(data)
+    else:
+        df = data
+    sns.countplot(data=df, x=x, hue=hue, **kwargs)
