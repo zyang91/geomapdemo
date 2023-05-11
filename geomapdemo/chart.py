b4e8684746365100b8cdd31fb744bcac8f2879cb
zyang91 <zyang91@binghamton.edu>
2023-05-11 12:51:46 -0400

    update new function

--- geomapdemo/chart.py
+++ geomapdemo/chart.py
@@ -13,7 +13,7 @@ def set_default_theme(style='darkgrid', **kwargs):
     sns.set_theme(style=style, **kwargs)
 
 def scatter_plot(data, x, y, hue= None, **kwargs):
-    '''Plot data and regression model fits across a FacetGrid.
+    '''Plot data and regression model fits across a FacetGrid. Used to plot a scatter chart
     Args:
         data (DataFrame): CSV file path or DataFrame object.
         x, y (str): Variables that specify positions on the x and y axes.
@@ -27,7 +27,7 @@ def scatter_plot(data, x, y, hue= None, **kwargs):
     sns.catplot(data=df, x=x, y=y, hue=hue, **kwargs)
 
 def violin_plot(data, x, y, hue= None, **kwargs):
-    '''Plot data and regression model fits across a FacetGrid.
+    '''Plot data and regression model fits across a FacetGrid. Used to plot a violin chart
     Args:
         data (DataFrame): CSV file path or DataFrame object.
         x, y (str): Variables that specify positions on the x and y axes.
@@ -41,7 +41,7 @@ def violin_plot(data, x, y, hue= None, **kwargs):
     sns.violinplot(data=df, x=x, y=y, hue=hue, **kwargs)
 
 def violin_spilt(data, x, y, hue, spilt= True, **kwargs):
-    '''Plot data and regression model fits across a FacetGrid.
+    '''Plot data and regression model fits across a FacetGrid. Used to plot a spilt violin chart
     Args:
         data (DataFrame): CSV file path or DataFrame object.
         x, y (str): Variables that specify positions on the x and y axes.
@@ -56,7 +56,7 @@ def violin_spilt(data, x, y, hue, spilt= True, **kwargs):
     sns.violinplot(data=df, x=x, y=y, hue=hue, split=spilt, **kwargs)
 
 def single_violin(data, variable, **kwargs):
-    '''Plot data and regression model fits across a FacetGrid.
+    '''Plot data and regression model fits across a FacetGrid. Used to plot a single violin chart
     Args:
         data (DataFrame): CSV file path or DataFrame object.
         variable (str): Variables that specify positions on the x and y axes.
@@ -69,7 +69,7 @@ def single_violin(data, variable, **kwargs):
     sns.violinplot(x=df[variable], **kwargs)
 
 def box_plot(data, x, y, hue= None, **kwargs):
-    '''Plot data and regression model fits across a FacetGrid.
+    '''Plot data and regression model fits across a FacetGrid. Used to plot a box chart
     Args:
         data (DataFrame): CSV file path or DataFrame object.
         x, y (str): Variables that specify positions on the x and y axes.
@@ -83,7 +83,7 @@ def box_plot(data, x, y, hue= None, **kwargs):
     sns.boxplot(data=df, x=x, y=y, hue=hue, **kwargs)
 
 def single_box_plot(data, variable, **kwargs):
-    '''Plot data and regression model fits across a FacetGrid.
+    '''Plot data and regression model fits across a FacetGrid. Used to plot a single box chart
     Aegs:
         data (DataFrame): CSV file path or DataFrame object.
         variable (str): Variables that specify positions on the x and y axes.
@@ -96,7 +96,7 @@ def single_box_plot(data, variable, **kwargs):
     sns.boxplot(data=df,x=df[variable], **kwargs)
 
 def bar_plot(data, x, y, hue= None, **kwargs):
-    '''Plot data and regression model fits across a FacetGrid.
+    '''Plot data and regression model fits across a FacetGrid. Used to plot a bar chart 
     Args:
         data (DataFrame): CSV file path or DataFrame object.
         x, y (str): Variables that specify positions on the x and y axes.
@@ -110,7 +110,7 @@ def bar_plot(data, x, y, hue= None, **kwargs):
     sns.barplot(data=df, x=x, y=y, hue=hue, **kwargs)
 
 def single_bar_plot(data, variable, **kwargs):
-    '''Plot data and regression model fits across a FacetGrid.
+    '''Plot data and regression model fits across a FacetGrid. Can be used to plot a single bar chart variable.(changable to bar_plot)
     Args:
         data (DataFrame): CSV file path or DataFrame object.
         variable (str): Variables that specify positions on the x and y axes.
@@ -123,7 +123,7 @@ def single_bar_plot(data, variable, **kwargs):
     sns.barplot(data=df,y=df[variable], **kwargs)
 
 def count_plot(data, x, hue= None, **kwargs):
-    '''Plot data and regression model fits across a FacetGrid.
+    '''Plot data and regression model fits across a FacetGrid. Can be used to plot a single variable.(changable to single_count_plot)
     Args:
         data (DataFrame): CSV file path or DataFrame object.
         x (str): Variables that specify positions on the x and y axes.
@@ -135,3 +135,16 @@ def count_plot(data, x, hue= None, **kwargs):
     else:
         df = data
     sns.countplot(data=df, x=x, hue=hue, **kwargs)
+
+def single_count_plot(data, variable, **kwargs):
+    '''Plot data and regression model fits across a FacetGrid. Can be used to plot a single variable.(changable to count_plot)
+    Args:
+        data (DataFrame): CSV file path or DataFrame object.
+        variable (str): Variables that specify positions on the x and y axes.
+        kwargs: Additional keyword arguments are passed to the function used to draw the plot on the joint Axes, superseding items in the joint_kws dictionary.
+    '''
+    if isinstance(data, str):
+        df = pd.read_csv(data)
+    else:
+        df = data
+    sns.countplot(data=df,x=df[variable], **kwargs)
