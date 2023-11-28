8c676ea9876e5839173374ca74755ffd31df223e
zyang91 <zyang91@binghamton.edu>
2023-11-27 22:57:32 -0500

    Bump version: 0.3.3 → 0.3.4

--- geomapdemo/__init__.py
+++ geomapdemo/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Zhanchao Yang"""
 __email__ = 'zyang91@binghamton.edu'
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 
 from .geomapdemo import *
--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/zyang91/geomapdemo',
-    version='0.3.3',
+    version='0.3.4',
     zip_safe=False,
 )
